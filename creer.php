e9eef77a4e862ae2ebb5a6f8c4eba7cfd7bdc0cb
AdelineBenoit <adeline_benoit@orange.fr>
2022-09-17 13:16:28 +0200

    update

---
+++ creer.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <link rel="stylesheet" href="resa.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>creer</h1>
+
+    <?php
+    include("en-tete.php");
+
+    $server = "localhost";
+    $user = "root";
+    $password = "root";
+    $bdd = "video";
+
+    $film = $_POST["titre"];
+    // var_dump($film);
+
+
+    try {
+        $connexion = new PDO('mysql:host=' . $server . ';dbname=' . $bdd, $user, $password);
+        $connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        // echo 'connecté à la base de données'.'<br>';
+        $resultat = $connexion->query($film);
+        $datas = $resultat->fetchAll();
+        // $resultat2 = $connexion->query($real);
+        // $datas2 = $resultat2->fetchAll();
+        // var_dump($datas);
+        $connexion = null;
+        var_dump($datas);
+
+
+    }
+        catch (PDOException $error) {
+            echo 'n° ' . $error->getCode() . '<br/>';
+            die('Erreur : ' . $error->getMessage() . '<br/>');
+        }
+
+
+
+    ?>
+    
+</body>
+</html>
--- form.php
+++ form.php
@@ -2,6 +2,8 @@
 <html lang="en">
 
 <head>
+
+    <link rel="stylesheet" href="resa.css">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -11,26 +13,44 @@
 
 <body style=" background: ivoire;">
 
-    <h1 style="color: blue;" >Saisie d'un nouveau film</h1>
 
-    <form action="" method="post">
-        <p>Identifiant :</p>
-        <input type="text" name=""> </br>
-        <p>Titre :</p>
-        <input type="text" name=""> </br>
-        <p>Type de film :</p>
-        <input type="text" name=""> </br>
-        <p>Réalisateur :</p>
-        <input type="text" name=""> </br>
-        <p>Année de sortie :</p>
-        <input type="text" name=""> </br>
-        <p>Affiche :</p>
-        <input type="text" name=""> </br>
-        <p>Résumé :</p>
-        <input type="text" name=""> </br>
-        <button type="submit">Créer</button>
-        <button>Recommencer</button>
-    </form>
+    <h1 class="titre">Saisie d'un nouveau film</h1>
+    <?php
+    include("en-tete.php");
+    ?>
+
+    <div>
+
+        <form action="creer.php" method="post">
+            <p>Identifiant :</p>
+            <input type="text" name="identifiant"> </br>
+            <p>Titre :</p>
+            <input type="text" name="titre"> </br>
+
+            
+            <!-- <input type="select"> -->
+            <libellé>Type de film</libellé> </br>
+            <select>
+            <option name="ANI">Animation</option>
+            <option name="HOR">Horreur</option>
+            <option name="COM">Comédie</option>
+            <option name="ACT">Action</option>
+            <option name="POL">Policier</option>
+            <option name="SCF">Science-Fiction</option>
+            </select>
+
+            <p>Réalisateur :</p>
+            <input type="select" name="realisateur"> </br>
+            <p>Année de sortie :</p>
+            <input type="text" name="annee"> </br>
+            <p>Affiche :</p>
+            <input type="text" name="affiche"> </br>
+            <p>Résumé :</p>
+            <input type="text" name="resume"> </br>
+            <input type="submit" name="Valider">
+            <button>Recommencer</button>
+        </form>
+    </div>
 </body>
 
 </html>
